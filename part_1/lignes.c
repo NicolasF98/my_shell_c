5fa9747a5f756db5ef627479593f6a1999dadc4c
Nicolas F <fournout.nicolas@gmail.com>
2019-04-20 15:26:10 +0200

    2nd commit

--- part_1/lignes.c
+++ part_1/lignes.c
@@ -1,27 +1,3 @@
-/*
-Interpréteur de commandes à but pédagogique
-Copyright (C) 2011-2018 Guillaume Huard
-Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
-
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
-GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
-
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
-
-Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
-	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
-*/
-
-
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -54,7 +30,7 @@ void decouper_ligne(char *ligne, char *ligne_decoupee[]) {
        A la fin d'un mot, on remplace le 1er espace qui le suit par '\0'
      */
 /**********************************************/
-    // A remplacer : cette solution temporaire ne découpe rien, elle ne
+    // A remplacer : cette solution temporaire ne dï¿½coupe rien, elle ne
     // fonctionne qu'avec les commandes sans argument
     ligne_decoupee[0] = ligne;
     ligne_decoupee[1] = NULL;
---
+++ part_1/report_part1.md
@@ -0,0 +1,27 @@
+## 1 - Boucle de lecture:
+
+#### *[a]:*
+
+Sur chaque ligne lue la fonction ` executer_ligne_commande ` découpe une ligne fournit en argument, si la commande est terminée on affiche le resultat et on expanse(changer le nom de la variable par sa valeur) se qu'il faut dans la ligne en question.
+
+#### *[b]:*
+
+    ``lire_ligne_fichier:`` 
+    Lit les lignes d'un fichier, en remplacant les `\n` par des `\0`, stock les modifications en écrasant la ligne.
+    On retourn 0 quand tout es lu et sinon 1.
+
+    ``trouver_et_appliquer_affectation_variable:``
+    On cherche une affectation de variable et on l'applique afin de remplacer le nom de la variable par sa valeure.
+
+    ``decouper_ligne:`` 
+    Chaque mot devient une chaine, on remplace tous les ` ` par des `\0`, la variable `ligne_decoupee` stock toutes les adresses mémoire de chaque début de mot.
+    Si la ligne est vide on retourn `NULL`.
+
+    
+
+    
+
+
+
+
+
